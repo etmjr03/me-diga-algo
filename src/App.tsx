e11a9569237cbca42969676d8ed18169b5b454cf
etmjr03 <etmjr03@gmail.com>
2022-12-15 23:35:15 -0300

    Adicionando validacao com conta Google

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { createContext, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 function App() {
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/rooms/new" element={<NewRoom />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/rooms/new" element={<NewRoom />}/>
+        </Routes>
     </BrowserRouter>
   );
 }
